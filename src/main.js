61be17d1075f2b0584c630e019c3741142d98827
hushicai <bluthcy@gmail.com>
2015-05-13 22:33:03 +0800

    update

--- src/main.js
+++ src/main.js
@@ -9,19 +9,22 @@ define(
         var canvas = {
             context: null,
             init: function (element) {
+                var context = element.getContext('2d');
+
                 var width = element.width;
                 var height = element.height;
 
-                element.style.width = width + 'px';
-                element.style.height = height + 'px';
-
-                element.width = devicePixelRatio * width;
-                element.height = devicePixelRatio * height;
-
-                var context = element.getContext('2d');
+                // http://www.html-js.com/article/2584
 
                 if (devicePixelRatio !== 1) {
+                    // scale up
+                    element.width = devicePixelRatio * width;
+                    element.height = devicePixelRatio * height;
                     context.scale(devicePixelRatio, devicePixelRatio);
+
+                    // scale down
+                    element.style.width = width + 'px';
+                    element.style.height = height + 'px';
                 }
 
                 this.context = context;
